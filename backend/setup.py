f3f97ee47079e501259852311ecd4221aec62834
Артем Романюков <vens148@gmail.com>
2022-09-26 20:13:16 +0700

    Добавил описание пакета в PyPi

--- backend/setup.py
+++ backend/setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
-version = '0.2.0'
+version = '0.2.1'
 
 packages = find_packages()
 
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='django-nearly-solid',
     version=version,
+    author="Artem Romanukov",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
     include_package_data=True,
     zip_safe=False,
     packages=packages,
--- backend/tasks.py
+++ backend/tasks.py
@@ -1,6 +1,8 @@
 from invoke import task, run
+from setup import version
 
 
 @task
 def build(ctx):
     run('python setup.py sdist --dist-dir ./build')
+    run(f'twine upload -r pypi build/django-nearly-solid-{version}.tar.gz')
